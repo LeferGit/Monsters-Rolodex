c78afe27d323ffd69d1ecf602b13949988ef7abb
LeferGit <arielaufer@gmail.com>
2020-03-25 16:42:13 +0200

    Updates

---
+++ precache-manifest.302e7e172390d20f878ab5a966aa757a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ede6faa4e013bc1490fda486f459a34",
+    "url": "/LeferGitnpm%20install%20-g%20serve/Monsters-Rolodex.git/index.html"
+  },
+  {
+    "revision": "ee3af850ee14838d4993",
+    "url": "/LeferGitnpm%20install%20-g%20serve/Monsters-Rolodex.git/static/css/main.1816d718.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/LeferGitnpm%20install%20-g%20serve/Monsters-Rolodex.git/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LeferGitnpm%20install%20-g%20serve/Monsters-Rolodex.git/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3af850ee14838d4993",
+    "url": "/LeferGitnpm%20install%20-g%20serve/Monsters-Rolodex.git/static/js/main.87c0d56f.chunk.js"
+  },
+  {
+    "revision": "9b6d083cdba604684315",
+    "url": "/LeferGitnpm%20install%20-g%20serve/Monsters-Rolodex.git/static/js/runtime-main.3d23e1d5.js"
+  }
+]);
