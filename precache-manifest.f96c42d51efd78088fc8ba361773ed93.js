e401d5e4a7afa4191a64524a8e093b314f759083
LeferGit <arielaufer@gmail.com>
2020-03-25 17:13:46 +0200

    Updates

---
+++ precache-manifest.f96c42d51efd78088fc8ba361773ed93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ede119444843c74b87fb94492ec9777",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "11872f1e75222e43a165",
+    "url": "/Monsters-Rolodex/static/css/main.1816d718.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/Monsters-Rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11872f1e75222e43a165",
+    "url": "/Monsters-Rolodex/static/js/main.341f04be.chunk.js"
+  },
+  {
+    "revision": "321d78d19afe0ae88472",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.564099f5.js"
+  }
+]);
