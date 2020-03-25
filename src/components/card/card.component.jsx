5be0f11e73bc8050b7da2c22910b75a05c96febf
LeferGit <arielaufer@gmail.com>
2020-03-25 17:16:04 +0200

    commits

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.style.css';
 export const Card = ({monster}) =>
 (
     <div className='card-container'>
-        <img alt="monster" src={'https://robohash.org/${monster.id}?set=set1&size=180x180'}></img>
+        <img alt="monster" src={'https://robohash.org/${monster.id}?set=set2&size=180x180'}></img>
         <h2> {monster.name } </h2>
         <p> {monster.email} </p>
     </div>
